26f88d7ecf3994b0f3eea8110144b165e28de112
Elena <elena.bozhinova.ied@gmail.com>
2022-11-02 21:57:38 +0100

    cas1

---
+++ cas1/cas-prv.js
@@ -0,0 +1,12 @@
+console.log('Hello Semos!');
+
+console.warn('This is a warning');
+
+console.error('Fatal ERROR');
+
+// komentar vo edNA LINIJA
+
+/* komentar vo povekje
+linii kod
+*/
+
---
+++ cas1/variables.js
@@ -0,0 +1,29 @@
+// var - globalni promenlivi (vo povekje fajlovi)
+// let - lokalna promenliva (vo ovoj file ili na odredena komanda)
+// const - konstanta, koja ne moze da bide promeneta odkoga ke bide zadadena
+
+let username;
+
+// let username = 'Kocho', emailAdress = 'test@test.com' - moze povekje let komandi vo edna linija podeleni so zapirka
+
+// let email_address emailAdress - za imenuvanje na promenlivi
+let emailAdress = 'test@test.com'
+
+// = zadava vrednost na promenliva ili konstanta
+username = 'Kocho'
+
+// let username = 'Kocho' - moze da se spojat komandi
+
+// consile.log() - pecatenje na vrednostite na promenlivite
+console.log(username)
+
+const a = 11;
+const b = 6;
+const zbir = a % b;
+
+// matematicki operacii + sobiranje - odzemanje *mnozenje / delenje % ostatok
+
+console.log(zbir)
+
+// se vo navodnici e tekst, i dokolku se pravi matematicka operacija so tekst sekogash se dobiva tekst
+const number = '10'
